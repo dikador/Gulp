8589a504b116817e3bed39e200b6d5afb916aea0
Dikador <dikador@mail.ru>
2021-03-27 12:40:51 +0600

    gulp

--- #src/js/popup.js
+++ #src/js/popup.js
@@ -4,7 +4,7 @@ const lockPadding = document.querySelectorAll('.lock-padding')
 
 let unlock = true;
 
-const timeout = 600;
+const timeout = 700;
 
 if (popupLinks.length > 0) {
     for (let index = 0; index < popupLinks.length; index++) {
